47ec050197ce2c6af262a968ad7fc31d311fc9b9
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-08-25 07:09:39 +0200

    .

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_6z3wqtr_k&rd$spyq1u+s8lzmv#8nr%(q+^t%rery+p=nbet-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".railway.app", # https://saas.prod.railway.app/
