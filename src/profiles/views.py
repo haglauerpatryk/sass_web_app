a95dd639d651a756772d100d3925c47d34004302
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2025-12-28 09:28:55 +0100

    Removing most obvious redundancies from the course

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -22,10 +21,6 @@ def profile_detail_view(request, username=None, *args, **kwargs):
         user.has_perm("subscriptions.pro"),
         user.has_perm("subscriptions.advanced"),    
     )
-    # user_groups = user.groups.all()
-    # print("user_groups", user_groups)
-    # if user_groups.filter(name__icontains='basic').exists():
-    #     return HttpResponse("Congrats")
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user
     context = {
